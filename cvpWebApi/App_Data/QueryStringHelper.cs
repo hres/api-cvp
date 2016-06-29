e824d5121576b2de99714478558e035b67e17296
dbeauvai <diane.beauvais@canada.ca>
2016-06-29 15:40:05 -0400

    includes reference page, helper functions, work on search/search results

---
+++ cvpWebApi/App_Data/QueryStringHelper.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace cvp
+{
+    /// <summary>
+    /// Summary description for Common
+    /// </summary>
+    public static class QueryStringHelper
+    {
+        public static string GetLang(this NameValueCollection queryString)
+        {
+            return queryString.AllKeys.Any(x => x.ToLower() == "lang") ? queryString["lang"].Trim() : string.Empty;
+        }
+
+        public static string GetGcpID(this NameValueCollection queryString)
+        {
+            return queryString.AllKeys.Any(x => x.ToLower() == "gcpid") ? queryString["gcpid"].Trim() : string.Empty;
+        }
+        public static string GetBrandName(this NameValueCollection queryString)
+        {
+            return queryString.AllKeys.Any(x => x.ToLower() == "brandname") ? queryString["brandName"].Trim() : string.Empty;
+        }
+        //public static string GetControlNumber(this NameValueCollection queryString)
+        //{
+        //    return queryString.AllKeys.Any(x => x.ToLower() == "contnum") ? queryString["contnum"].Trim() : string.Empty;
+        //}
+        //public static string GetRegion(this NameValueCollection queryString)
+        //{
+        //    return queryString.AllKeys.Any(x => x.ToLower() == "region") ? queryString["region"].Trim() : string.Empty;
+        //}
+        //public static string GetDrugName(this NameValueCollection queryString)
+        //{
+        //    return queryString.AllKeys.Any(x => x.ToLower() == "drugname") ? queryString["drugName"].Trim() : string.Empty;
+        //}
+        //public static string GetTrialPhase(this NameValueCollection queryString)
+        //{
+        //    return queryString.AllKeys.Any(x => x.ToLower() == "phase") ? queryString["phase"].Trim() : string.Empty;
+        //}
+        //public static string GetInsStartDate(this NameValueCollection queryString)
+        //{
+        //    return queryString.AllKeys.Any(x => x.ToLower() == "startdate") ? queryString["startDate"].Trim() : string.Empty;
+        //}
+        //public static string GetRating(this NameValueCollection queryString)
+        //{
+        //    return queryString.AllKeys.Any(x => x.ToLower() == "rating") ? queryString["rating"].Trim() : string.Empty;
+        //}
+        //public static string GetInspectionType(this NameValueCollection queryString)
+        //{
+        //    return queryString.AllKeys.Any(x => x.ToLower() == "instype") ? queryString["insType"].Trim() : string.Empty;
+        //}
+        //public static string GetDownloadExcel(this NameValueCollection queryString)
+        //{
+        //    return queryString.AllKeys.Any(x => x.ToLower() == "excel") ? queryString["excel"].Trim() : string.Empty;
+        //}
+        
+    }
+}
--- cvpWebApi/Controllers/HomeController.cs
+++ cvpWebApi/Controllers/HomeController.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GoC.WebTemplate;
+using cvpWebApi.Models;
+using System.Diagnostics;
+using cvp;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
 
 namespace cvpWebApi.Controllers
 {
     public class HomeController : GoC.WebTemplate.WebTemplateBaseController
     {
+        DBConnection dbConnection = new DBConnection("en");
         public ActionResult IndexEN()
         {
             // Page Title
@@ -106,6 +112,151 @@ namespace cvpWebApi.Controllers
             }
             else //Default redirect to English Index
                 return Redirect("IndexEN");
+        }
+
+        public ActionResult SearchReport()
+        {
+            // Page Title
+            this.WebTemplateCore.HeaderTitle = "Canada Vigilance Adverse Reaction Database";
+
+            // Breadcrumb Navigtation
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://canada.ca/en/index.html", "Home", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://open.canada.ca/en", "Open Government", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://open.canada.ca/data/en/dataset?q=DPD", "Open Data", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("", "Search Canada Vigilance Adverse Reaction Database", ""));
+
+            // Feedback
+            this.WebTemplateCore.ShowFeedbackLink = true;
+
+            // Social Media Links
+            this.WebTemplateCore.ShowSharePageLink = true;
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.bitly);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.linkedin);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.blogger);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.myspace);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.delicious);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.pinterest);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.digg);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.reddit);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.diigo);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.stumbleupon);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.email);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.tumblr);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.facebook);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.twitter);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.gmail);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.yahoomail);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.googleplus);
+
+            // Date Modified
+            this.WebTemplateCore.DateModified = Convert.ToDateTime("2016-04-13");
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SearchResultEn()
+        {
+            Debug.WriteLine("im in the SearchResultsEn method in the HOmeController");
+            string brandname = Request.QueryString["brandName"].Trim();
+            string reportsString = string.Empty;
+
+            // Page Title
+            this.WebTemplateCore.HeaderTitle = "Canada Vigilance Adverse Reaction Database";
+
+            // Breadcrumb Navigation
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://canada.ca/en/index.html", "Home", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://open.canada.ca/en", "Open Government", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://open.canada.ca/data/en/dataset?q=DPD", "Open Data", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("", "Search Canada Vigilance Adverse Reaction Database", ""));
+
+            // Feedback
+            this.WebTemplateCore.ShowFeedbackLink = true;
+
+            // Social Media Links
+            this.WebTemplateCore.ShowSharePageLink = true;
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.bitly);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.linkedin);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.blogger);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.myspace);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.delicious);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.pinterest);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.digg);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.reddit);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.diigo);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.stumbleupon);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.email);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.tumblr);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.facebook);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.twitter);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.gmail);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.yahoomail);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.googleplus);
+
+            // Date Modified
+            this.WebTemplateCore.DateModified = Convert.ToDateTime("2016-04-13");
+
+            if (Request.IsAjaxRequest())
+            {
+                Debug.WriteLine(brandname);
+                List<Report> reports = new List<Report>();
+                reports = dbConnection.GetReportByDrugName(brandname);
+                Debug.WriteLine(reports.Count);
+
+                //JavaScriptSerializer js = new JavaScriptSerializer();
+                //Context.Response.Write(js.Serialize(reports));
+
+                reportsString = JsonConvert.SerializeObject(reports);
+                //Debug.WriteLine(reportsString);
+
+                //context.Response.ContentType = "application/json; charset=utf-8";
+                //context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                //context.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+                return Json(reportsString);
+                //return View();
+            }
+            else
+                return View();
+
+        }
+
+        public ActionResult Reference()
+        {
+            // Page Title
+            this.WebTemplateCore.HeaderTitle = "Canada Vigilance Adverse Reaction Online Database - Reference";
+
+            // Breadcrumb Navigtation
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://canada.ca/en/index.html", "Home", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://open.canada.ca/en", "Open Government", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("http://open.canada.ca/data/en/dataset?q=DPD", "Open Data", ""));
+            this.WebTemplateCore.Breadcrumbs.Add(new Breadcrumb("", "Search Canada Vigilance Adverse Reaction Online Database", ""));
+
+            // Feedback
+            this.WebTemplateCore.ShowFeedbackLink = true;
+
+            // Social Media Links
+            this.WebTemplateCore.ShowSharePageLink = true;
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.bitly);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.linkedin);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.blogger);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.myspace);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.delicious);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.pinterest);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.digg);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.reddit);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.diigo);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.stumbleupon);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.email);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.tumblr);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.facebook);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.twitter);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.gmail);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.yahoomail);
+            this.WebTemplateCore.SharePageMediaSites.Add(GoC.WebTemplate.Core.SocialMediaSites.googleplus);
+
+            // Date Modified
+            this.WebTemplateCore.DateModified = Convert.ToDateTime("2016-06-07");
+            return View();
         }
     }
 }
